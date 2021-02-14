faf0696593989c035bfe623c23e003b601c61b1c
Renato Rodrigues <spamcares-github@yahoo.com>
2021-02-14 13:36:19 -0300

    deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t renators/multi-client:latest -t renators/multi-client:$SHA -f ./client/Dockerfile.dev ./client
-docker build -t renators/multi-server:latest -t renators/multi-server:$SHA -f ./server/Dockerfile.dev ./server
-docker build -t renators/multi-worker:latest -t renators/multi-worker:$SHA -f ./worker/Dockerfile.dev ./worker
+docker build -t renators/multi-client:latest -t renators/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t renators/multi-server:latest -t renators/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t renators/multi-worker:latest -t renators/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push renators/multi-client:latest
 docker push renators/multi-server:latest
@@ -12,6 +12,6 @@ docker push renators/multi-worker:$SHA
 
 kublectl apply -f k8s
 
-kublectl apply set image deployments/client-deployment client=renators/multi-client:$SHA
-kublectl apply set image deployments/server-deployment server=renators/multi-server:$SHA
-kublectl apply set image deployments/worker-deployment worker=renators/multi-worker:$SHA
+kublectl set image deployments/client-deployment client=renators/multi-client:$SHA
+kublectl set image deployments/server-deployment server=renators/multi-server:$SHA
+kublectl set image deployments/worker-deployment worker=renators/multi-worker:$SHA
